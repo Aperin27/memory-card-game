81f6efbd30c19223ea18dac5863c0ea4b01d85e0
Segun Ajibola <arperin27@gmail.com>
2026-03-15 00:23:25 +0200

    Initial quiz website

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const EMOJIS = ["🚀", "🎧", "🍓", "🌙", "⚡", "🎨", "🧠", "🦋"];
+const EMOJIS = ["🍍", "🦶", "👃", "🐧", "🐈", "🍪", "☕", "🏸"];
 
 function shuffle(array) {
   const copy = [...array];
